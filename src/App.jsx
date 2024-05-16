1dde1de41fb3f57dbe30dae038f6dfa938a88b9d
Awarth <adarshtiwari8808@gmail.com>
2024-05-16 10:44:03 +0530

    first push

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import "./App.css";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { useEffect } from "react";
+import { login, logout } from "./store/authSlice";
+import { Footer, Header } from "./components";
+import { Outlet } from "react-router-dom";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  });
+
+  return !loading ? 
+    <>
+      <div className="min-h-screen min-w-full  flex flex-wrap flex-col bg-black text-white ">
+        <Header />
+        <>TODO : {Outlet}</>
+        <Footer />
+      </div>
+    </>
+   : null;
+}
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,41 @@
+import Header from "./Header/Header.jsx";
+import Footer from "./Footer/Footer.jsx";
+import Container from "./container/Container.jsx";
+import Logo from "./Logo.jsx";
+import LogoutBtn from "./Header/LogoutBtn.jsx";
+import Select from "./Select.jsx";
+import RTE from "./RTE.jsx";
+import PostForm from "./postForm/PostForm.jsx";
+import Button from "./Button.jsx";
+import AuthLayout from "./AuthLayout.jsx";
+import Home from "../pages/Home.jsx";
+import LoginPage from "../pages/LoginPage.jsx";
+import SignupPage from "../pages/SignupPage.jsx";
+import AllPosts from "../pages/AllPosts.jsx";
+import AddPost from "../pages/AddPost.jsx";
+import EditPost from "../pages/EditPost.jsx";
+import Post from "../pages/Post.jsx";
+import Input from "./Input.jsx";
+import PostCard from "./Postcard.jsx"
+
+export{
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    Select,
+    RTE,
+    PostForm,
+    Button,
+    AuthLayout,
+    Home,
+    LoginPage,
+    SignupPage,
+    AllPosts,
+    AddPost,
+    EditPost,
+    Post,
+    Input,
+    PostCard
+}
